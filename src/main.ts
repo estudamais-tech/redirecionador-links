ff4cf4808935a5fb1e85d7a04c2d9853d6615f49
vitorugo1 <hvitor79@gmail.com>
2025-07-15 20:36:18 -0300

    configurações de dependências

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/go/:destination', (req: Request, res: Response) => {
+  const destination = req.params.destination;
+
+  console.log(`Redirecting for: ${destination}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
