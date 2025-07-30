cb55d354a0304dda59d5a649f02d2be27cb7e08f
vitorugo1 <hvitor79@gmail.com>
2025-07-29 23:56:30 -0300

    correção: logava o prefetch na base de dados, agora só loga redirecionamentos reais

--- src/main.ts
+++ src/main.ts
@@ -13,17 +13,30 @@ app.get('/go/:destinationName', async (req: Request, res: Response) => {
   const userIp = req.ip || 'IP desconhecido';
   try {
     console.log(`[Route] Recebida requisição para: ${destinationName}`);
-    //serviço para obtenção da url pelo nome
+    //serviço para obtenção da url e id pelo nome
     const targetUrl = await getUrlByName(destinationName);
     const urlId = await getIdByName(destinationName);
 
     if (targetUrl) {
-      console.log(`[Route] URL encontrada: ${targetUrl}. Redirecionando...`);
-      // Chama o log. É async, mas não precisa esperar (fire-and-forget)
-      logClick({
-        linkId: urlId,
-        ip: userIp,
-      });
+      //Evita que log o prefetch na db
+      const isPrefetch = req.headers['sec-purpose'] === 'prefetch';
+
+      if (isPrefetch) {
+        // Se for um prefetch, nós logamos no console para saber que aconteceu,
+        // mas NÃO chamamos o serviço para registrar no banco de dados.
+        console.log(
+          `[Prefetch] Requisição de prefetch detectada para: ${destinationName}. Clique não será registrado.`,
+        );
+      } else {
+        // Se for uma requisição normal (clique ou Enter), registramos o clique.
+        console.log(`[Route] URL encontrada: ${targetUrl}. Redirecionando...`);
+        // Chama o log. É async, mas não precisa esperar (fire-and-forget)
+        logClick({
+          linkId: urlId,
+          ip: userIp,
+        });
+      }
+
       return res.redirect(302, targetUrl);
     } else {
       //FUTURAMENTE REDIRECIONAR PARA 404 PADRÃO DA PLATAFORMA
